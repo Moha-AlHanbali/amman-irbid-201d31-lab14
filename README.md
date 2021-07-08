737a25d644295af8b0cc7ced88d825a7a32d1fdb
Moha-AlHanbali <Moha.AlHanbali@gmail.com>
2021-07-08 09:08:25 +0300

    used everyones code for live preview version

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # amman-201d19-lab14
+
+```
+
+THIS WORK WAS CONTRIBED BY
+- Mohammed Al-Hanbali
+- Trad Al Hariri
+- Dua Jaradat
+
+IT IS RE-PUSHED ON THIS REPO FOR THE SAKE OF LIVE PREVIEW ONLY
+THE ORIGINAL WORK CAN BE FOUND AT https://github.com/Moha-AlHanbali/amman-irbid-201d31/tree/main/class-14/lab/starter-code
+
+```
--- js/catalog.js
+++ js/catalog.js
@@ -1,5 +1,17 @@
 /* global Product, Cart */
 
+
+
+/* THIS WORK WAS CONTRIBED BY
+- Mohammed Al-Hanbali
+- Trad Al Hariri
+- Dua Jaradat
+
+IT IS RE-PUSHED ON THIS REPO FOR THE SAKE OF LIVE PREVIEW ONLY
+THE ORIGINAL WORK CAN BE FOUND AT https://github.com/Moha-AlHanbali/amman-irbid-201d31/tree/main/class-14/lab/starter-code
+*/
+
+
 'use strict';
 
 // Set up an empty cart for use on this page.
@@ -8,10 +20,13 @@ const cart = new Cart([]);
 // On screen load, we call this method to put all of the busmall options
 // (the things in the Product.allProducts array) into the drop down list.
 function populateForm() {
-
+  // let catalog = document.getElementById('catalog');
   //TODO: Add an <option> tag inside the form's select for each product
   const selectElement = document.getElementById('items');
   for (let i in Product.allProducts) {
+    let optionEl = document.createElement('option');
+    optionEl.textContent = Product.allProducts[i].name; 
+    selectElement.appendChild(optionEl);
 
   }
 
@@ -24,8 +39,10 @@ function handleSubmit(event) {
 
   // TODO: Prevent the page from reloading
 
+    event.preventDefault();
+
   // Do all the things ...
-  addSelectedItemToCart();
+  addSelectedItemToCart(event);
   cart.saveToLocalStorage();
   updateCounter();
   updateCartPreview();
@@ -35,17 +52,46 @@ function handleSubmit(event) {
 // TODO: Add the selected item and quantity to the cart
 function addSelectedItemToCart() {
   // TODO: suss out the item picked from the select list
+
+  const selectElement = document.getElementById('items');
+  let item = selectElement.options[selectElement.selectedIndex].text;
+  console.log(item);
+
+  
   // TODO: get the quantity
+  let itemQuantityEl = document.getElementById('quantity');
+  let itemQuantity = itemQuantityEl.value;
+      console.log(itemQuantity);
   // TODO: using those, add one item to the Cart
+  cart.addItem(item, itemQuantity);
 }
 
 // TODO: Update the cart count in the header nav with the number of items in the Cart
-function updateCounter() {}
+function updateCounter() {
+  let itemCount = document.getElementById('itemCount');
+  itemCount.textContent = cart.items.length;
+}
 
 // TODO: As you add items into the cart, show them (item & quantity) in the cart preview div
+
+let cartContents = document.getElementById('cartContents');
+let itemsUL = document.createElement('ul')
+cartContents.appendChild(itemsUL);
+
 function updateCartPreview() {
   // TODO: Get the item and quantity from the form
+
+  const selectElement = document.getElementById('items');
+  let item = selectElement.options[selectElement.selectedIndex].text;
+  let itemQuantityEl = document.getElementById('quantity');
+  let itemQuantity = itemQuantityEl.value;
+
   // TODO: Add a new element to the cartContents div with that information
+  let itemsList = document.createElement('li');
+  
+  itemsList.textContent = `${item} was added ${itemQuantity} times.`
+
+  itemsUL.appendChild(itemsList);
 }
 
 // Set up the "submit" event listener on the form.
